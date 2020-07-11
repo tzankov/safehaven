67beed75f4e3ceab8d6d834c74ee26dad154fe4e
Tzankov <tzankotz@hotmail.com>
2020-07-11 14:11:28 +0100

    added testing for listing, uploading and downloading in AWS

--- tests/test_safehaven.py
+++ tests/test_safehaven.py
@@ -7,16 +7,23 @@ from moto import mock_s3
 from safehaven import Aws
 
 @mock_s3
-def test_upload():
+def aws_test_list_upload():
     #Create the bucket in Moto's 'virtual' AWS account
     conn = boto3.resource('s3', region_name='eu-west-1')
     conn.create_bucket(Bucket='s3-shared-object-store-test')
-    #Create an instance of the Aws class and upload a test object
+    #Create an instance of the Aws class and upload a single entry list of a test object
+    files = ['test_file']
     s3 = Aws()
-    s3.create_client('s3-shared-object-store-test', 'test_file', 'region', 'aws_access_key_id', 'aws_secret_access_key')
-    s3.list_objects()
+    s3.create_client('s3-shared-object-store-test', files, 'region', 'aws_access_key_id', 'aws_secret_access_key')
     s3.upload_objects()
 
-    body = conn.Object('s3-shared-object-store-test', 'test_file').get()['Body'].read().decode("utf-8")
+    #List objects in newly created bucket and assert that the single entry list we uploaded earlier matches the what is in the `cloud`
+    objects = s3.list_objects()
+    assert objects == ['test_file']
+    print("AWS List / Upload Test PASSED")
 
-    assert body == 'exists'
+    #Download object and assert
+    s3.download_objects()
+    print("AWS Download Test PASSED")
+
+aws_test_list_upload()
